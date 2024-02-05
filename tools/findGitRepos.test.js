ddc9b42ca98834c65d008907e18734327c4921df
aidanbek <a.kalymbekov>
2024-02-05 22:19:26 +0600

    fix test

--- tools/findGitRepos.test.js
+++ tools/findGitRepos.test.js
@@ -2,46 +2,55 @@ const extract = require('./findGitRepos');
 const mock = require('mock-fs');
 const GIT = '.git'
 
-mock({
-    empty_folder: {},
-    dir_as_git_repo: {
-        [GIT]: {}
-    },
-    nested_dir: {
-        dir1: {
-            dir11: {
-                [GIT]: {},
-                other_folder: {}
-            },
-            other_folder: {}
-        },
-        dir2: {
-            [GIT]: {},
-            other_folder: {}
-        },
-        dir3: {
-            other_folder: {}
-        },
-        other_folder: {}
-    }
-})
-
-afterAll(() => mock.restore())
+afterEach(() => mock.restore())
 
 describe('find git repo', () => {
     it('no git repo in empty dir', () => {
+        mock({
+            'node_modules': mock.load('node_modules'),
+            empty_folder: {},
+        })
+
         const actual = extract('empty_folder')
 
         expect(actual).toEqual([])
     })
 
     it('passed dir as git repo', () => {
+        mock({
+            'node_modules': mock.load('node_modules'),
+            dir_as_git_repo: {
+                [GIT]: {}
+            },
+        })
+
         const actual = extract('dir_as_git_repo')
 
         expect(actual).toEqual(['dir_as_git_repo'])
     })
 
     it('nested dirs as git repos', () => {
+        mock({
+            'node_modules': mock.load('node_modules'),
+            nested_dir: {
+                dir1: {
+                    dir11: {
+                        [GIT]: {},
+                        other_folder: {}
+                    },
+                    other_folder: {}
+                },
+                dir2: {
+                    [GIT]: {},
+                    other_folder: {}
+                },
+                dir3: {
+                    other_folder: {}
+                },
+                other_folder: {}
+            }
+        })
+
         const actual = extract('nested_dir')
 
         expect(actual).toEqual(['nested_dir\\dir1\\dir11', 'nested_dir\\dir2'])
